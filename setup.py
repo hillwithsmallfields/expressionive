df840e9fb4a64ef3b3ddc686b1eaddd94176a2c0
John Sturdy <jcg.sturdy@gmail.com>
2026-02-13 10:02:37 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="expressionive",
+    version="0.0.11",
+    description="A Python3-compatible fork of 'Throw out your templates', similar to Nevow's 'stan'.",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+)
