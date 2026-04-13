a839dd2efc4c1b040515d753c500268968505ab3
John C. G. Sturdy <jcg.sturdy@gmail.com>
2026-04-13 15:14:05 +0100

    change hard-coded title to a parameter, and allow a stylesheet file to be passed in

--- src/expressionive/exprpages.py
+++ src/expressionive/exprpages.py
@@ -5,21 +5,27 @@ import expressionive.expressionive as xpres
 from expressionive.expressionive import htmltags as T
 
 def page_text(page_contents,
-              style_text,
-              script_text):
+              title=None,
+              style_text="",
+              script_text="",
+              stylesheet=None,
+              ):
     """Return the text of an expressionive page.
 
     The page contents are given as an expressionive expression,
     and the stylesheet and scripts as strings."""
+    head = [T.meta(charset='utf-8')]
+    if stylesheet:
+        head.append(T.meta(rel='stylesheet',
+                           type_='text/css',
+                           href=stylesheet))
+    if title:
+        head.append(T.title[title])
     return xpres.Serializer(xpres.examples_vmap, 'utf-8').serialize(
         xpres.HTML5Doc([xpres.safe_unicode(style_text
                                            + script_text),
                         page_contents],
-                       head=T.head[T.meta(charset='utf-8'),
-                                   T.meta(rel='stylesheet',
-                                          type_='text/css',
-                                          href="dashboard.css"),
-                                   T.title["Personal dashboard"]]))
+                       head=T.head[head]))
 
 def file_contents(filename):
     """Return the contents of a file."""
@@ -29,7 +35,7 @@ def file_contents(filename):
     return f"File {filename} not found"
 
 def tagged(tag, text):
-    """Surround a some text with opening and closing HTML tags."""
+    """Surround some text with opening and closing HTML tags."""
     return "<" + tag + ">" + text + "</" + tag + ">"
 
 def tagged_file_contents(tag, filename):
